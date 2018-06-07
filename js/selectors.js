49fd52c6f662cdfadeb7267c0872d63367402f7a
Murat Celik <cashkl@yandex.com>
2018-06-07 22:09:54 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,6 +3,15 @@ function paragraphSelector() {
   return $('p')
 }
 function lastImageSelector(){
-  return $('.img:last')
+  return $('img:last')
 
+}
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+function divSelector() {
+  return $('.pics')
+}
+function firsLastItem() {
+  return $('ul:fist-child #pic-list')
 }
